86f9931d64ecf8883dd1f5be10a1c4598e58d71c
fimaAidarous <fma0899@gmail.com>
2024-06-26 09:40:59 -0700

    complete sign in page functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -31,7 +31,6 @@ export const signin = async (req,res,next) => {
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
       .json(rest);
-     // .json(validUser);
     } catch (error) {
         next(error);
     }
