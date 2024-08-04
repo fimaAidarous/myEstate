3173efd276248c9041d61db82f92ea25f2b723c7
fimaAidarous <fma0899@gmail.com>
2024-08-04 12:05:04 -0700

    Create search API route?

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"; 
+import mongoose from "mongoose";
 import Listing from "../models/listingModel.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -12,7 +12,7 @@ export const createListing = async (req, res, next) => {
 };
 
 export const deleteListing = async (req, res, next) => {
-  let listing; 
+  let listing;
   try {
     listing = await Listing.findById(req.params.id);
   } catch (error) {
@@ -20,7 +20,7 @@ export const deleteListing = async (req, res, next) => {
       return next(errorHandler(404, "Listing not found!")); // <--- Added this line
     }
     return next(error);
-  } 
+  }
 
   if (!listing) {
     return next(errorHandler(404, "Listing not found!"));
@@ -47,7 +47,7 @@ export const updateListing = async (req, res, next) => {
       return next(errorHandler(404, "Listing not found!")); // <--- Added this line
     }
     return next(error);
-  } 
+  }
 
   if (!listing) {
     return next(errorHandler(404, "Listing not found!"));
@@ -69,21 +69,67 @@ export const updateListing = async (req, res, next) => {
   }
 };
 
-
-
 export const getListing = async (req, res, next) => {
-  let listing; 
-  try { 
-   listing = await Listing.findById(req.params.id);
+  let listing;
+  try {
+    listing = await Listing.findById(req.params.id);
   } catch (error) {
-    if (error instanceof mongoose.Error.CastError) { 
-      return next(errorHandler(404, 'Listing not found!')); 
+    if (error instanceof mongoose.Error.CastError) {
+      return next(errorHandler(404, "Listing not found!"));
     }
-    return next(error); 
-  } 
+    return next(error);
+  }
   if (!listing) {
-    return next(errorHandler(404, 'Listing not found!'));
+    return next(errorHandler(404, "Listing not found!"));
   }
   res.status(200).json(listing);
 };
 
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.furnished;
+
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.furnished;
+
+    if (type === undefined || type === "false") {
+      type = { $in: ['sale', 'rent'] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || 'createdAt';
+    const order = req.query.order || 'desc';
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: 'i' },
+      offer,
+      furnished,
+      parking,
+      type,
+    }).sort(
+    {[sort]: order}
+  ).limit(limit).skip(startIndex);
+  return res.status(200).json(listings);
+
+  } catch (error) {
+    next(error);
+  }
+};
