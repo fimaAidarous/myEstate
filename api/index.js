e32f53079241932deeb1eb41c3ec52ac2f78b65d
fimaAidarous <fma0899@gmail.com>
2024-08-13 09:17:39 -0700

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/userRoute.js';
 import authRouter from './routes/authRoute.js';
 import listingRouter from './routes/listingRoute.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -14,6 +15,9 @@ mongoose.connect(process.env.MONGO)
 .catch((err) => {
     console.log(err);
 });
+
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -28,6 +32,12 @@ app.listen(1000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  });
 
 app.use(( err,req,res,next) => {
     const statusCode = err.statusCode || 500;
